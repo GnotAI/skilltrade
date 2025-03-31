187191d7da7c450592e54d1793876d3c0129fefc
GnotAI <alleluiabriggs@gmail.com>
2025-03-31 10:27:28 +0100

    add: password hashing

---
+++ utils/hash/hash.go
@@ -0,0 +1,20 @@
+package hash
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashes a plaintext password using bcrypt.
+func HashPassword(password string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedBytes), nil
+}
+
+// ComparePassword compares a hashed password with a plaintext password.
+func ComparePassword(hashedPassword, plainPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil
+}
